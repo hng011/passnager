e4b8cce10a2c165e842f33bfd1d731136bc43273
hng011 <hansnaufal.3040@gmail.com>
2024-02-05 12:30:42 +0700

    adding functions to append and show data

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import csv
+import art
 from dotenv import load_dotenv
 from tabulate import tabulate
 from passnager_modules.PassCrypt import pass_encrypt, pass_decrypt
@@ -8,44 +9,67 @@ from passnager_modules.PassCrypt import pass_encrypt, pass_decrypt
 class Passnager:
     def __init__(self, file_name: str="data.csv"):
         self.__data = []
+        self.__file_name = file_name
         # Open file
         while True:
             try:
-                with open(file_name, "r") as f:
-                    print(f"Read {file_name} file")
+                with open(self.__file_name, "r") as f:
                     for row in csv.reader(f):
                         self.__data.append(row)
                     break
             except FileNotFoundError:
-                # Only in development: delete this when you're already in production
-                dummy = [
-                    {"no":1,"username_or_email":"dummy1","password":"dummy1"},
-                    {"no":2,"username_or_email":"dummy2","password":"dummy2"}
-                ]
-                print(f"Create {file_name} file")
+                print(f"Create {self.__file_name} file")
                 headers: list = ["no", "username_or_email", "password"]
                 with open("./data.csv", "w") as f:
                     writer = csv.DictWriter(f, fieldnames=headers)
                     writer.writeheader()
-                    writer.writerows(dummy)
+                    # writer.writerows(dummy)
                 continue
 
     def show_data(self, table_fmt="fancy_grid") -> tabulate:
         try:
+            if not self.__data:
+                return "0 Data Found"
+            
+            print(len(self.__data[1:]), "data found")
             return tabulate(self.__data[1:], headers=self.__data[0], tablefmt=table_fmt)
         except:
             return "Something went wrong while trying to read a csv file"
 
-    def create_data():
+    def __add_list_data(self) -> tuple:
+        if len(self.__data) > 1:
+            no = int(self.__data[-1][0]) + 1
+        else:
+            no = 1
+        username = input("\nUsername/email\t: ")
+        password = input("Password\t: ")
+        return (no, username, password)
+
+    def add_data(self) -> None:
+        try:
+            new_data = self.__add_list_data()
+            if len(self.__data) > 1:
+                list_username = [x[1] for x in self.__data[1:]]
+            else:
+                list_username = []
+
+            with open(self.__file_name, "a") as f:
+                # Abort when the username is already exists
+                if new_data[1] in list_username:
+                    print("\nData already exists!!!\n")
+                else:
+                    writer = csv.writer(f)
+                    writer.writerow(new_data)
+                    self.__data.append(new_data)
+                    print("++ Successfully adding data\n")
+
+        except Exception as e:
+            sys.exit(e)
+
+    def update_data(self):
         ...
 
-    def read_data():
-        ...
-
-    def update_data():
-        ...
-
-    def delete_data():
+    def delete_data(self):
         ...
 
 def get_dotenv() -> dict:
@@ -60,11 +84,59 @@ def get_dotenv() -> dict:
         sys.exit(e)
     return envs
 
-def main():
-    pm = Passnager()
+def clear_screen():
+    if os.name == "nt":
+        os.system("cls") 
+    else:
+        os.system("clear")
 
-    # test
-    print(pm.show_data())
+def back_to_menu():
+    while True:
+        if input("0 for back\t: ") == "0":
+            clear_screen()
+        break
+    
+def main():
+    clear_screen()
+    pm = Passnager()
+    print("""
+====================================
+      | WELCOME TO PASSNAGER | 
+++==+++==+++==+++==+++==+++==+++==++
+    [1] show data
+    [2] add data       
+    [0] Exit       
+====================================""")
+
+    display = """
+====================================         
+         |   PASSNAGER   |
+++==+++==+++==+++==+++==+++==+++==++
+    [1] show data
+    [2] add data       
+    [0] Exit       
+===================================="""
+
+    while True:
+        choice = input("[*] Menu Choice: ")
+        print()
+        match choice:
+            case "1":
+                print(pm.show_data())
+                back_to_menu()
+            case "2":
+                pm.add_data()
+                back_to_menu()
+            case "0":
+                clear_screen()
+                print(art.text2art('SEE YOU LATER', font='rand'))
+                input("Press anything")
+                sys.exit()
+            case _:
+                print("Invalid Input!!!\n")
+                continue
+
+        print(display)
 
 if __name__ == "__main__":
     main()
