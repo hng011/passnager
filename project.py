7fb45615776679e25636dd9335233fc5f43d6e98
hng011 <hansnaufal.3040@gmail.com>
2024-02-05 10:51:49 +0700

    adding a Passnager class and read/write csv file function

--- project.py
+++ project.py
@@ -1,19 +1,52 @@
 import os
 import sys
+import csv
 from dotenv import load_dotenv
+from tabulate import tabulate
 from passnager_modules.PassCrypt import pass_encrypt, pass_decrypt
 
-def create_data():
-    ...
+class Passnager:
+    def __init__(self, file_name: str="data.csv"):
+        self.__data = []
+        # Open file
+        while True:
+            try:
+                with open(file_name, "r") as f:
+                    print(f"Read {file_name} file")
+                    for row in csv.reader(f):
+                        self.__data.append(row)
+                    break
+            except FileNotFoundError:
+                # Only in development: delete this when you're already in production
+                dummy = [
+                    {"no":1,"username_or_email":"dummy1","password":"dummy1"},
+                    {"no":2,"username_or_email":"dummy2","password":"dummy2"}
+                ]
+                print(f"Create {file_name} file")
+                headers: list = ["no", "username_or_email", "password"]
+                with open("./data.csv", "w") as f:
+                    writer = csv.DictWriter(f, fieldnames=headers)
+                    writer.writeheader()
+                    writer.writerows(dummy)
+                continue
 
-def read_data():
-    ...
+    def show_data(self, table_fmt="fancy_grid") -> tabulate:
+        try:
+            return tabulate(self.__data[1:], headers=self.__data[0], tablefmt=table_fmt)
+        except:
+            return "Something went wrong while trying to read a csv file"
 
-def update_data():
-    ...
+    def create_data():
+        ...
 
-def delete_data():
-    ...
+    def read_data():
+        ...
+
+    def update_data():
+        ...
+
+    def delete_data():
+        ...
 
 def get_dotenv() -> dict:
     # load .env
@@ -28,11 +61,10 @@ def get_dotenv() -> dict:
     return envs
 
 def main():
-    pass_encrypt()
-    pass_decrypt()
-    envs = get_dotenv()
-    print(envs["app_name"])
-    print(envs["secret_key"])
+    pm = Passnager()
+
+    # test
+    print(pm.show_data())
 
 if __name__ == "__main__":
     main()
