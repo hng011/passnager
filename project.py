8703ca91a5056f11823fd2bc16f3216e2df38a74
hng011 <hansnaufal.3040@gmail.com>
2024-02-05 22:38:11 +0700

    passnager v1.0

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import csv
-import art
+import pyfiglet
 from random import randint
 from dotenv import load_dotenv
 from tabulate import tabulate
@@ -24,9 +24,9 @@ class Passnager:
                     break
             except FileNotFoundError:
                 print(f"Create {self.__file_name} file")
-                headers: list = ["id", "app", "username_or_email", "password"]
-                with open("./data.csv", "w") as f:
-                    writer = csv.DictWriter(f, fieldnames=headers)
+                header: list = ["id", "app", "username_or_email", "password"]
+                with open(self.__file_name, "w") as f:
+                    writer = csv.DictWriter(f, fieldnames=header)
                     writer.writeheader()
                 continue
 
@@ -36,16 +36,27 @@ class Passnager:
     def __decrypt_pass(self, encrypted_pwd=None):
         return pass_decrypt(encrypted_pwd, self.__secret_key)
 
-    def show_data(self, table_fmt="fancy_grid") -> tabulate:
+    def __sort_data(self, data: list) -> list:
+        return sorted(data, key=lambda x: x[1])
+
+    def show_data(self, *data, table_fmt="fancy_grid") -> tabulate:
+        df=None
+
+        if len(data) == 0:
+            df = self.__data
+        else:
+            df = data[0]
+
         try:            
-            print(len(self.__data[1:]), "data found")
+            print(len(df[1:]), "data found")
             decrypted_data = []
-            for x in self.__data[1:]:
+
+            for x in df[1:]:
                 decrypted_data.append([x[0], x[1], x[2], self.__decrypt_pass(x[3])])
 
-            return tabulate(decrypted_data, headers=self.__data[0], tablefmt=table_fmt)
-        except:
-            return "Something went wrong while trying to read a csv file"
+            return tabulate(self.__sort_data(decrypted_data), headers=df[0], tablefmt=table_fmt)
+        except Exception as e:
+            return f"Something went wrong while trying to read the csv file\n{e}"
 
     def __generate_id(val_len=4) -> str:
         id = ""
@@ -77,7 +88,6 @@ class Passnager:
             else:
                 print("Each field required!!!")
 
-
     def add_data(self) -> None:
         try:
             new_data = self.__add_list_data()
@@ -88,27 +98,78 @@ class Passnager:
                 list_app = []
                 list_username = []
 
-            with open(self.__file_name, "a") as f:
-                # Abort when the username is already exists
-                if (
-                    new_data[1] in list_app and
-                    new_data[2] in list_username
-                ):
-                    print("\nData already exists!!!\n")
+            while True:
+                is_commited = input("Save (y/n) ").lower() 
+                if is_commited == "n":
+                    return 0
+                elif is_commited == "y":                    
+                    with open(self.__file_name, "a") as f:
+                        # Abort when the username is already exists
+                        if (
+                            new_data[1] in list_app and
+                            new_data[2] in list_username
+                        ):
+                            print("\nData already exist!!!\n")
+                            return 1
+                        else:
+                            writer = csv.writer(f)
+                            writer.writerow(new_data)
+                            self.__data.append(new_data)
+                            print("Successfully adding data ++\n")
+                            return 1
                 else:
-                    writer = csv.writer(f)
-                    writer.writerow(new_data)
-                    self.__data.append(new_data)
-                    print("++ Successfully adding data\n")
+                    print("Invalid input!!!")
+                    continue
 
         except Exception as e:
             sys.exit(e)
 
-    def update_data(self):
-        ...
-
     def delete_data(self):
-        ...
+        temp_data = []
+        with open(self.__file_name, "r") as f:
+            for row in csv.reader(f):
+                temp_data.append(row)
+
+        if len(temp_data) < 2:
+            print("No data available")
+            input("Press enter to continue...")
+        else:
+            print(self.show_data(temp_data))
+            while True:
+                id = input("Type one id to be deleted (0 for back): ")
+                if id == "0": 
+                    break
+                else:
+                    try:
+                        for x in temp_data:
+                            if id in x:
+                                temp_data.pop(temp_data.index(x))
+                                
+                                # Make a dict
+                                dict_temp_data = []
+                                header = temp_data[0] 
+                                for row in temp_data[1:]:
+                                    dict_temp_data.append(
+                                        {
+                                            header[0]: row[0], 
+                                            header[1]: row[1],
+                                            header[2]: row[2],
+                                            header[3]: row[3]
+                                        }
+                                    )                        
+
+                                # Store
+                                with open(self.__file_name, "w") as f:
+                                    writer = csv.DictWriter(f, fieldnames=header)
+                                    writer.writeheader()
+                                    writer.writerows(dict_temp_data)
+
+                                    self.__data = temp_data
+                                    input(f"\nSuccessfully deleting data with id [{id}] -- | press enter to continue")
+                                return
+                        print("id not found")        
+                    except Exception as e:
+                        print(e)
 
 def generate_key():
     key = generate_secret_key()
@@ -119,7 +180,7 @@ def generate_key():
     except Exception as e:
         sys.exit(e)
 
-    sys.exit("Succesfully generating a secret key")
+    sys.exit("Secret key generated successfully++")
 
 def get_dotenv() -> dict:
     if os.environ["SECRET_KEY"] == "":
@@ -143,7 +204,6 @@ def back_to_menu():
     while True:
         if not(input("0 for back\t: ") == "0"): continue    
         break
-
     clear_screen()
     
 def main():
@@ -151,6 +211,7 @@ def main():
         generate_key()
 
     envs = get_dotenv()
+    clear_screen()
     pm = Passnager(secret_key=envs["secret_key"])
 
     print("""
@@ -159,6 +220,7 @@ def main():
     ++==+++==+++==+++==+++==+++==+++==++
         [1] show data
         [2] add data       
+        [3] delete data       
         [0] Exit       
     ====================================""")
 
@@ -168,12 +230,13 @@ def main():
     ++==+++==+++==+++==+++==+++==+++==++
         [1] show data
         [2] add data       
+        [3] delete data       
         [0] Exit       
     ===================================="""
 
     while True:
         try:
-            choice = input("[*] Menu Choice: ")
+            choice = input("\n[*] Menu Choice: ")
         except EOFError:
             choice = "0"
         print()
@@ -182,19 +245,23 @@ def main():
                 print(pm.show_data())
                 back_to_menu()
             case "2":
-                pm.add_data()
-                back_to_menu()
+                res = pm.add_data()
+                if res == 1:
+                    back_to_menu()
+            case "3":
+                pm.delete_data()
             case "0":
                 clear_screen()
-                print(art.text2art('SEE YOU LATER', font='rand'))
-                input("Press anything")
+                print(pyfiglet.figlet_format("seeyou",font='banner3-D'))
+                input("Press enter to continue")
+                clear_screen()
                 sys.exit()
             case _:
                 print("Invalid Input!!!\n")
-                input("Press anything")
-                clear_screen()
+                input("Press enter to continue")
                 continue
 
+        clear_screen()
         print(display)
 
 if __name__ == "__main__":
